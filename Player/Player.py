85c56ec0189e07c3decf8f87fe329178d6c8f504
Matt Hollands <mh739@cam.ac.uk>
2016-01-09 15:28:22 -0500

    moved simulate into its own file

--- Player/Player.py
+++ Player/Player.py
@@ -4,6 +4,7 @@ import sys
 import time
 import PokerPhysics as PP
 import Pokerini
+import Simulation
 
 class Player:
     #when both players agree with bet it is moved into the pot
@@ -281,7 +282,7 @@ class Player:
             return "RAISE:"+str(maxRaise)
 
         if(self.numBoardCards >= 3):
-            winChance = self.simulate(100)
+            winChance = Simulation.simulate(self.myHand, self.boardCards, self.numBoardCards, 100)
             print winChance
             if winChance < 0.4:
                 return self.checkFold(canCheck)
@@ -291,61 +292,6 @@ class Player:
                 return "RAISE:"+str(maxRaise)
             raiseTo = int(minRaise + (0.8-winChance)*(maxRaise - minRaise)/0.2)
             return "RAISE:"+str(raiseTo)
-
-        #Picks random cards to fill out the table and runs multiple simulations to find an approximation for the win probability
-    def simulate(self, numSimulations):
-        
-        if(self.numBoardCards == 3):
-            wins = 0
-            for x in range(0,numSimulations):
-                cardSet = set(self.boardCards)
-                for card in self.myHand: cardSet.add(card)
-                newCards = []
-                for i in range(0,6):
-                    card = PP.pickRandomCard(cardSet)
-                    cardSet.add(card)
-                    newCards.append(card)
-                fakeBoard = self.boardCards + newCards[0:2]
-                fakeOpponent = newCards[2:]
-                myBest = PP.findBestHand(self.myHand, fakeBoard)
-                opponentBest = PP.findBestHand(fakeOpponent, fakeBoard)
-                if PP.isBetterHand(myBest[0], opponentBest[0]) == 1 : wins+=1
-            winPercentage = 1.0*wins/numSimulations
-            return winPercentage
-
-        if(self.numBoardCards == 4):
-            wins = 0
-            for x in range(0,numSimulations):
-                cardSet = set(self.boardCards)
-                for card in self.myHand: cardSet.add(card)
-                newCards = []
-                for i in range(0,5):
-                    card = PP.pickRandomCard(cardSet)
-                    cardSet.add(card)
-                    newCards.append(card)
-                fakeBoard = self.boardCards + newCards[0:1]
-                fakeOpponent = newCards[1:]
-                myBest = PP.findBestHand(self.myHand, fakeBoard)
-                opponentBest = PP.findBestHand(fakeOpponent, fakeBoard)
-                if PP.isBetterHand(myBest[0], opponentBest[0]) == 1 : wins+=1
-            winPercentage = 1.0*wins/numSimulations
-            return winPercentage
-
-        if(self.numBoardCards == 5):
-            wins = 0
-            myBest = PP.findBestHand(self.myHand, self.boardCards)
-            for x in range(0,numSimulations):
-                cardSet = set(self.boardCards)
-                for card in self.myHand: cardSet.add(card)
-                fakeOpponent = []
-                for i in range(0,4):
-                    card = PP.pickRandomCard(cardSet)
-                    cardSet.add(card)
-                    fakeOpponent.append(card)
-                opponentBest = PP.findBestHand(fakeOpponent, self.boardCards)
-                if PP.isBetterHand(myBest[0], opponentBest[0]) == 1 : wins+=1
-            winPercentage = 1.0*wins/numSimulations
-            return winPercentage
 
     def checkFold(self, canCheck):
         if(canCheck):
